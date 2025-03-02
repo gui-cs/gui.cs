3666298316b4b8bcb276ecce7d6fac6982633517
Tig <tig@users.noreply.github.com>
2025-03-02 13:39:56 -0700

    tweaked script

--- Scripts/testloop.sh
+++ Scripts/testloop.sh
@@ -10,7 +10,7 @@ iterationCount=1
 while true; do
     echo "Starting iteration $iterationCount..."
 
-    dotnet test --no-build --diag:log.txt
+    dotnet test --no-build --diag:TestResults/log.txt
 
     if [ $? -ne 0 ]; then
         echo "Test run failed on iteration $iterationCount. Exiting."
